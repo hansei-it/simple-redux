05b6948fb1fabcae452f8fb8c131dedc26ebde5b
hansei-it <armyvision@hansei.ac.kr>
2023-12-12 00:40:56 +0900

    createStore() 사용이 이제 권장되지 않아. @reduxjs/toolkit 설치 후 configureStore 사용
    npm i @reduxjs/toolkit 설치 후
    코드 수정.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {createStore} from 'redux';
+//import {createStore} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+//const store = createStore(rootReducer);
+// Redux Toolkit을 사용하여 스토어 생성
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
